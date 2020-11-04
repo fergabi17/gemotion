48bb6e199281536642a09ea7791ca6b1ef6b47de
Fernanda Cetnarovski <fernanda.cetnarovski@smartbox.com>
2020-11-04 15:07:10 +0000

    Add game search functionality (on API)

--- games/urls.py
+++ games/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='games')
+    path('', views.index, name='games'),
+    path('game_list/', views.search_game, name='game_list')
 ]
--- games/views.py
+++ games/views.py
@@ -1,8 +1,43 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# FOR GAME SEARCH
+import http.client
+import urllib.parse
+import json
+
+
 def index(request):
     """
     Creates a view for the index
     """
     return render(request, 'games/index.html')
+
+
+def search_game(request):
+    """
+    """
+
+
+    game_title = request.POST['game-title']
+    game_title = urllib.parse.quote(game_title)
+
+    conn = http.client.HTTPSConnection("rapidapi.p.rapidapi.com")
+
+    headers = {
+        'x-rapidapi-key': "e930d07731mshe00718d0d6136eap1ae483jsnf6cea31e0367",
+        'x-rapidapi-host': "rawg-video-games-database.p.rapidapi.com"
+        }
+
+    conn.request("GET", f"/games?search={game_title}", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+    result = json.loads(data)["results"]
+    
+    
+    context = {
+        "content": result
+    }
+    
+    return render(request, 'games/game-list.html', context)
