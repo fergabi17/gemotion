2fda6895972f15ea0b17486a9e2bae1a2ed81869
Fernanda Cetnarovski <fernanda.cetnarovski@smartbox.com>
2020-11-07 17:23:53 +0000

    Add tool to create fixtures from csv

---
+++ resources/fixture-builder/fixtures_from_csv.py
@@ -0,0 +1,42 @@
+import csv
+import sys
+import json
+
+def csv_to_dict(csv_file):
+    """
+    @csv_file: file
+    @field_name: string
+    @pattern: regex
+    This method extracts info from a field on a csv
+    validated by a pattern. It returns an array of dicts 
+    containing the info, skipping duplicates
+    """
+    final_array = []
+    reader = csv.DictReader(csv_file)
+    emotions = "emotions"
+    categories = "categories"
+    pk = 1
+    for row in reader:
+        emotion = row[emotions]
+        category = row[categories]
+
+        fixture = {
+            "pk": pk,
+            "model": "reviews.emotion",
+            "fields": {
+                "name": str(emotion),
+                "category": int(category)
+            }
+        }
+        
+        final_array.append(fixture)
+        pk = pk +1
+        
+    return final_array
+
+csv_file = open('resources/fixture-builder/emotions.csv', 'r', encoding='utf-8-sig')
+list_of_dicts = csv_to_dict(csv_file)
+
+
+with open('reviews/fixtures/emotions.json', 'w') as json_file:
+    json.dump(list_of_dicts, json_file)
