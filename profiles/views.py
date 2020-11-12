b74d45645297d81251537d6aab8cff1db859f315
Fernanda Cetnarovski <fernanda.cetnarovski@smartbox.com>
2020-11-12 17:22:14 +0000

    Add anonymous reviews

--- profiles/views.py
+++ profiles/views.py
@@ -13,31 +13,45 @@ def profile(request):
     Creates a view for the user's profile
     presenting data about reviews
     """
-    profile = get_object_or_404(UserProfile, user=request.user)
-    reviews = profile.reviews.all()
+    if request.user.is_authenticated:
+        profile = get_object_or_404(UserProfile, user=request.user)
+        reviews = profile.reviews.all()
 
-    games = reviews.values_list('game__name', flat=True).distinct()
+        games = reviews.values_list('game__name', flat=True).distinct()
+        games_reviewed = len(games)
 
-    top_emotions = reviews.values_list('emotion__name').annotate(
-        emotion_count=Count('emotion')).order_by('-emotion_count')
-    games_top_emotions = reviews.filter(emotion__name=top_emotions[0][0])
-    games_top_emotions = games_top_emotions.values_list('game__name', flat=True).distinct()
-    
-    top_categories = reviews.values_list('emotion__category__name').annotate(
-        category_count=Count('emotion__category__name')).order_by('-category_count')
-    games_top_categories = reviews.filter(emotion__category__name=top_categories[0][0])
-    games_top_categories = games_top_categories.values_list('game__name', flat=True).distinct()
-    
-    last_review = reviews.order_by('-date').first()
-    
+        top_emotions = reviews.values_list('emotion__name').annotate(
+            emotion_count=Count('emotion')).order_by('-emotion_count')
+        top_emotion = top_emotions[0][0]
+        games_top_emotions = reviews.filter(emotion__name=top_emotions[0][0])
+        games_top_emotions = games_top_emotions.values_list('game__name', flat=True).distinct()
+        
+        
+        top_categories = reviews.values_list('emotion__category__name').annotate(
+            category_count=Count('emotion__category__name')).order_by('-category_count')
+        top_category = top_categories[0][0]
+        games_top_categories = reviews.filter(emotion__category__name=top_categories[0][0])
+        games_top_categories = games_top_categories.values_list('game__name', flat=True).distinct()
+        
+        last_review = reviews.order_by('-date').first()
+    else:
+        profile = "user"
+        games_reviewed = "?"
+        games = "?"
+        top_emotion = "unknown"
+        games_top_emotions = "?"
+        top_category = "secret"
+        games_top_categories = "?"
+        last_review = "maybe Candy Crush"
+        
     context = {
         'profile': profile,
         
-        'reviews': len(games),
+        'reviews': games_reviewed,
         'game_names': games,
-        'top_emotion': top_emotions[0][0],
+        'top_emotion': top_emotion,
         'games_top_emotions': games_top_emotions,
-        'top_category': top_categories[0][0],
+        'top_category': top_category,
         'games_top_category': games_top_categories,
         'last_review': last_review
     }
--- reviews/views.py
+++ reviews/views.py
@@ -16,10 +16,11 @@ def review(request):
     Gets the game object and
     Creates a view for the review page
     """
-       
+
     if "term" in request.GET:
         em = Emotion.objects.filter(name__icontains=request.GET.get('term'))
-        cat = Category.objects.filter(name__istartswith=request.GET.get('term'))
+        cat = Category.objects.filter(
+            name__istartswith=request.GET.get('term'))
         emotions = list()
         for emotion in em:
             emotions.append(emotion.name)
@@ -29,11 +30,11 @@ def review(request):
     else:
         game = get_or_add_game(request)
         form = ReviewForm(request.POST)
-        
+
         emotions_model = Emotion.objects.all()
         emotions_model = serializers.serialize("json", emotions_model)
         emotions_model = json.loads(emotions_model)
-        
+
         emotions_pk = {}
         for emotion in emotions_model:
             emotions_pk[emotion['fields']['name']] = emotion['pk']
@@ -43,7 +44,7 @@ def review(request):
         for category in categories_model:
             categories_array.append(str(category))
         categories = json.dumps(categories_array)
-        
+
         context = {
             'game': game,
             'emotions': json.dumps(emotions_pk),
@@ -52,31 +53,35 @@ def review(request):
         }
 
         return render(request, 'reviews/review.html', context)
-    
+
 
 def post_review(request):
     """
     Posts all the emotions as single inputs in the database
     """
     result = request.POST['pk_list']
-    if result == "":
-        messages.error(request, "You can't post an empty review!")
-    else:
-        result = json.loads(result)
-        game_id = request.POST['game-id']
-        user_played = request.POST.get('played')
-        
+
+    result = json.loads(result)
+    game_id = request.POST['game-id']
+    user_played = request.POST.get('played')
+
+    if request.user.is_authenticated:
         profile = get_object_or_404(UserProfile, user=request.user)
         game = get_object_or_404(Game, pk=game_id)
         for pk in result:
             emotion = get_object_or_404(Emotion, pk=pk)
-            new_review = Review(game=game, 
-                            user_profile=profile,
-                            played=user_played,
-                            emotion=emotion)
+            new_review = Review(game=game,
+                                user_profile=profile,
+                                played=user_played,
+                                emotion=emotion)
             new_review.save()
-
+    else:
+        game = get_object_or_404(Game, pk=game_id)
+        for pk in result:
+            emotion = get_object_or_404(Emotion, pk=pk)
+            new_review = Review(game=game,
+                                played=user_played,
+                                emotion=emotion)
+            new_review.save()
     return redirect(reverse('profile'))
-    
-
 
